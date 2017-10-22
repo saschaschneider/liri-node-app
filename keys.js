8ef218fe5e398f14baa0d9713c84ca18f5ff84ee
saschaschneider <s4schnei@gmail.com>
2017-10-22 13:04:36 -0700

    V1

---
+++ keys.js
@@ -0,0 +1,15 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'b0Lh0TB1vkXOCzVqHWqBkhvLj',
+  consumer_secret: 'psZumHWJubVDsEpDZF0YnKzpuGUc9j0TueXtjXJYq2kAvHI1AX',
+  access_token_key: '921212819600678912-IRR36xeQUlZsRz36IWPjHH7fLRcCK6m',
+  access_token_secret: 'f808JDOLIdcSvMAIN9AQwOXcRJIScuDHDAVmvOTqdnjUt',
+}
+
+module.exports = twitterKeys;
+
+
+
+
+
